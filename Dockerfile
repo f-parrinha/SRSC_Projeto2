81b4848ff2f659a88ed0eb6d0fa731c5fce84cd9
Martin <m.magdalinchev@campus.fct.unl.pt>
2023-11-16 20:22:43 +0000

    project initialized with spring initializer

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# Get image with java installed
-FROM openjdk:17-jdk-slim
-
-# Set work dir
-WORKDIR /app
-
-# Copy jar to workdir
-COPY target/*jar-with-dependencies.jar /app
---
+++ proj2/src/test/java/com/srsc5817258360/proj2/Proj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srsc5817258360.proj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
